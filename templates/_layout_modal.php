9b9e60fa2dd655ae6ef8a3f02d6d4ff16350c4d0
lmadhuranga <lilan.maduranga@gmail.com>
2015-06-11 23:06:44 +0530

    removed the views template file bz they not using for rest generator

--- templates/_layout_modal.php
+++
@@ -1,5 +0,0 @@
-<?php $this->load->view('test/components/page_head',array('meta_title'=>$meta_title,'site_name'=>$site_name)); ?>
-
-<?php $this->load->view($sub_view,$result); // Subview is set in controller ?>
-
-<?php $this->load->view('test/components/page_tail',array('meta_title'=>$meta_title,'site_name'=>$site_name)); ?>
